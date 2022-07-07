3a4a68ea9fe77461d666c1c5dd3e4f8d92e98704
mousa <khodemousa@gmail.com>
2022-07-07 21:55:21 +0430

    input added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./css/index.css";
 import Input from "./components/Input";
+import Keyboard from "./components/Keyboard";
 
 function App() {
   return (
-    <div className="mx-auto flex flex-col justify-around">
+    <div className="mx-auto flex flex-col justify-around w-80 mt-20">
       <Input />
-      {/* <Keyboard /> */}
+      <Keyboard />
     </div>
   );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 function Input(props) {
-    return ();
+  return (
+    <div className="relative">
+      <div className="bg-gray-100 py-3 px-5 text-xl border-2 border-red-100 w-full overflow-hidden">
+        1234aljsdhf lkjhadlkfjshdflsjkl
+      </div>
+      <span className="absolute top-3 right-2 text-blue-800 font-bold text-2xl">
+        +
+      </span>
+    </div>
+  );
 }
-
 
 export default Input;
